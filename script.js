dea8a7758681c585dcaf7082090f1c032c02a63e
NN <43994113+NekrasovaNatalya@users.noreply.github.com>
2022-09-27 13:07:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const placeholders = document.querySelectorAll('.placeholder');
 
 item.addEventListener('dragstart', dragstart);
 item.addEventListener('dragend', dragend);
+
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragover);
+    placeholder.addEventListener('dragenter', dragenter);
+    placeholder.addEventListener('dragleave', dragleave);
+    placeholder.addEventListener('drop', dragdrop);
+}
