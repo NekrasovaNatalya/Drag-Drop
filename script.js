6ee4463f3ba5d7de49d0e3c574fe383f7591f51d
NN <43994113+NekrasovaNatalya@users.noreply.github.com>
2022-09-27 13:10:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,7 @@ function dragstart(event) {
     event.target.classList.add('hold');
     setTimeout(() => event.target.classList.add('hide'), 0);
 }
+
+function dragend(event) {
+    event.target.classList.remove('hold','hide');
+}
